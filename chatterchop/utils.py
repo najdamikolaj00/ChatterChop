df7c6bc88d5faa5b9a1daedd9fa38d8c42691119
Mikolaj Najda <najda.mikolaj00@gmail.com>
2023-09-06 12:02:12 +0200

    transcript normalization for forced alignment utility

--- chatterchop/utils.py
+++ chatterchop/utils.py
@@ -1,8 +1,11 @@
 """This file contains functions additional utilities for a package."""
 
 from typing import List
+import re
+import torch
+import torchaudio
 
-def func_tempalte(value: int, text: str, values: list) -> str:
+def func_template(value: int, text: str, values: list) -> str:
     """
     Function description
 
@@ -16,6 +19,61 @@ def func_tempalte(value: int, text: str, values: list) -> str:
 
     """
     return 'a'
+
+def load_audio_tc(audio_path: str) -> tuple(torch.Tensor, int):
+    """
+    Loading audio when working with torch.
+
+    Args:
+        audio_path (str): Path to audio file.
+
+    Returns:
+        tuple(torch.Tensor, int): Waveform and sample rate of audio file.
+
+    """
+
+    waveform, sample_rate = torchaudio.load(audio_path)
+    return waveform, sample_rate
+
+def waveform_resample_tc(waveform: torch.Tensor, sample_rate: int, desired_sample_rate: int = 16000) -> torch.Tensor:
+    """
+    Waveform resample function when working with torch.
+
+    Args:
+        waveform (torch.Tensor): Waveform as torch tensor.
+        sample_rate (int): Original sample rate of waveform.
+        desired_sample_rate (int): Desired sample rate for waveform.
+
+    Returns:
+        torch.Tensor: Resampled waveform to the desired sample rate.
+
+    """
+    transform = torchaudio.transforms.Resample(orig_freq = sample_rate,
+                                                new_freq= desired_sample_rate)
+    resampled_waveform = transform(waveform)
+
+    return resampled_waveform
+
+def check_cuda_availability():
+    """
+    Checking if cuda is available.
+
+    Args:
+        None
+
+    Returns:
+        device
+
+    """
+    if torch.cuda.is_available():
+        print("CUDA is available.")
+        device = torch.device("cuda")
+        print("Using GPU:", torch.cuda.get_device_name(0))
+    else:
+        print("CUDA is not available. Using CPU.")
+        device = torch.device("cpu")
+
+    return device
 
 def text_normalization(text: str) -> str:
     """
@@ -37,6 +95,27 @@ def text_normalization(text: str) -> str:
     one_long_sentence = ' '.join(text.split())
 
     return one_long_sentence
+
+def normalize_transcript_CTC(transcript: str) -> str:
+    """
+    This function performs text normalization for forced alignment.
+    Example: 
+        transcript: "I had that curiosity beside me at thit moment."
+        normalized: "I|HAD|THAT|CURIOSITY|BESIDE|ME|AT|THIS|MOMENT"
+
+    Args:
+        transcript (str): Transcript to normalize.
+
+    Returns:
+        str: Normalized transcript prepared for forced alignment.
+
+    *To do: change numbers to text
+        
+    """
+    normalized_transcript = re.sub(r'(\w*)[^\w\s]+(\w*)', r'\1 \2', transcript)
+    normalized_transcript = '|'.join(word.upper() for word in normalized_transcript.split())
+
+    return normalized_transcript
 
 def min_of_three(a: any, b: any, c: any) -> any:
     """
